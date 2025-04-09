85e12c9d09a9dbeab699a60319648631076dc61b
PavelBozhek <themolodets2570@gmail.com>
2025-04-10 00:06:24 +0500

    Test

--- internal/repositories/solution_repository.go
+++ internal/repositories/solution_repository.go
@@ -1,7 +1,9 @@
 package repositories
 
 import (
+	"log/slog"
 	"time"
+
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
@@ -16,8 +18,9 @@ func NewPgSolutionRepository(connection *sqlx.DB) *PgSolutionRepository {
 
 func (r *PgSolutionRepository) CreateSolution(taskId, userId, answerId uuid.UUID, isCorrect bool, subbmittedAt time.Time, answer *string) error {
 	query := `insert into t_solution (id_task, id_user, id_answer, is_correct, submitted_at, answer) values ($1, $2, $3, $4, $5, $6)`
-	_, err := r.db.Exec(query, taskId, userId, answerId, isCorrect, subbmittedAt, answer) 
+	_, err := r.db.Exec(query, taskId, userId, answerId, isCorrect, subbmittedAt, answer)
 	if err != nil {
+		slog.Error("Error writing solution to database", "error", err)
 		return err
 	}
 
